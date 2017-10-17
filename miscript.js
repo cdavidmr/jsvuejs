7e2b96dc6cd92e6fdfac76c85782284a2ae078f2
cdavidmr <cdavidmr@outlook.com>
2017-10-17 00:01:53 -0500

    hello javascript

--- miscript.js
+++ miscript.js
@@ -1,5 +1,5 @@
 var miTitulo = document.querySelector('h1');
-miTitulo.innerHTML = 'Hello world!';
+miTitulo.innerHTML = 'Hello world javascript!';
 
 function alerta() {
     alert("Esto es una alerta");

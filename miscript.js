bbcd728f8c04ad9b20a3a918cd6a5a97d7e63179
cdavidmr <cdavidmr@outlook.com>
2017-10-17 19:03:06 -0500

    ...

--- miscript.js
+++ miscript.js
@@ -3,6 +3,3 @@ console.log("Hola Mundo!");
 function alerta() {
     alert("Esto es una alerta");
 }
-
-var miTitulo = document.querySelector('h1');
-miTitulo.innerHTML = 'Hello world js!<br>git push origin';

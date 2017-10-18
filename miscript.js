a338fcfb86bc9ba3c38b7d763b550b987ba8657e
cdavidmr <cdavidmr@outlook.com>
2017-10-18 01:11:32 -0500

    ......

--- miscript.js
+++ miscript.js
@@ -1,7 +1,7 @@
 console.log('Hello world!')
 console.log('Learning js')
 console.log(1+19)
-console.log(Math.sqrt(9))
+console.log(Math.sqrt(25))
 function alerta() {
     alert("Esto es una alerta");
     var miTitulo = document.querySelector('h1');

f5326bf7117ac11d44acad277f7ef71f3fc2addf
cdavidmr <cdavidmr@outlook.com>
2017-10-17 19:04:29 -0500

    ....

--- miscript.js
+++ miscript.js
@@ -1,5 +1,5 @@
-console.log("Hola Mundo!");
-
 function alerta() {
     alert("Esto es una alerta");
+    var miTitulo = document.querySelector('h1');
+    miTitulo.innerHTML = 'Hello world js!';
 }

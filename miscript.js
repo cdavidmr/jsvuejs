2a7e427a9564852792957e9bd17f362ad90193bd
cdavidmr <cdavidmr@outlook.com>
2017-10-18 21:28:00 -0500

    remove .gitlab-ci.yml

--- miscript.js
+++ miscript.js
@@ -1,10 +1,1 @@
-console.log('update 5!')
-console.log('gitlab')
-console.log(1+19)
-console.log(Math.sqrt(25))
-function alerta() {
-    alert("Actualizado");
-    var miTitulo = document.querySelector('h1');
-    miTitulo.innerHTML = 'Hello Gitlab!';
-}
 

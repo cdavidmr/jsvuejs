1387e9b15ec3312e771d9927d4b6c3c6b71a84a9
cdavidmr <cdavidmr@outlook.com>
2017-10-18 20:29:37 -0500

    hello gitlab!

--- miscript.js
+++ miscript.js
@@ -5,5 +5,5 @@ console.log(Math.sqrt(25))
 function alerta() {
     alert("Esto es una alerta");
     var miTitulo = document.querySelector('h1');
-    miTitulo.innerHTML = 'Hello world js!';
+    miTitulo.innerHTML = 'Hello Gitlab!';
 }
